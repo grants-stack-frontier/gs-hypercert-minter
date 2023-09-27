81086db1e3f6042a1f6bdf9c019700c7325b8cd1
memosys <82053242+memosys@users.noreply.github.com>
2023-09-27 09:29:19 +0530

    styled chain switcher

---
+++ src/components/ChainSwitcher.tsx
@@ -0,0 +1,91 @@
+import {
+    Box,
+    MenuItem,
+    Modal,
+    ModalBody,
+    ModalCloseButton,
+    ModalContent,
+    ModalHeader,
+    ModalOverlay,
+    useDisclosure
+} from '@chakra-ui/react';
+import { useNetwork } from "wagmi";
+import { useWallets } from '@privy-io/react-auth';
+import { useEffect, useState } from 'react';
+import type { Chain } from "wagmi";
+
+export default function ChainSwitcher(){
+    
+const [chainName, setChainName] = useState<string>("" as string);
+const { chains, chain } = useNetwork();
+
+  useEffect(() => {
+    if (chain) {
+      setChainName(chain.name);
+    }
+  }, [chain]);
+  
+    const { wallets } = useWallets();
+    const { onOpen, onClose, isOpen } = useDisclosure();
+
+    return(
+        <>
+          <MenuItem
+              onClick={onOpen}
+              bg={'transparent'}
+              
+            rounded={'md'}
+            _hover={{bgColor:'green', color:'dark-green', fontWeight:'medium'}}
+            >
+              
+                Switch Network
+              
+            </MenuItem>
+            <MenuItem
+            bg={'transparent'}
+            rounded={'md'}
+            _hover={{bgColor:'green', color:'dark-green', fontWeight:'medium'}}>
+            
+               Connected to: {chainName ? chainName : "Loading..."}
+            </MenuItem>
+        <Modal closeOnOverlayClick={false} isOpen={isOpen} onClose={onClose}>
+        <ModalOverlay bg="blackAlpha.300" backdropFilter="blur(20px)" />
+        <ModalContent background={"#242423"} rounded={"2xl"} p={2} color={'white'} fontWeight={'medium'}>
+          <ModalHeader>Select a Network</ModalHeader>
+          <ModalCloseButton />
+          <ModalBody>
+          
+            {chains &&
+              chains.map((chainItem: Chain) => {
+                if ( chainItem.name === chainName) {
+                  return null;
+                }
+                return (
+                  <MenuItem
+                    key={chainItem.name}
+                    onClick={async () => {
+                      if (wallets && wallets[0]) {
+                        try {
+                          await wallets[0].switchChain(chainItem.id);
+                          onClose();
+                        } catch (error) {
+                          console.log(error);
+                        }
+                      }
+                    }}
+                    p={2}
+                    rounded={'md'}
+                    
+                    
+                    _hover={{bgColor:'green', color:'dark-green', fontWeight:'medium'}}
+                  >
+                    {chainItem.name}
+                  </MenuItem>
+                );
+              })}
+          </ModalBody>
+        </ModalContent>
+      </Modal>
+      </>
+    )
+}
--- src/components/PrivyAuthButton.tsx
+++ src/components/PrivyAuthButton.tsx
@@ -7,51 +7,34 @@ import {
   MenuButton,
   MenuItem,
   MenuList,
-  Popover,
-  PopoverTrigger,
-  PopoverContent,
-  PopoverBody,
-  PopoverArrow,
-  PopoverHeader,
-  Portal,
-  PopoverCloseButton,
 } from "@chakra-ui/react";
-import { useEffect, useState } from "react";
 import { usePrivy } from "@privy-io/react-auth";
 import { useCopyToClipboard } from "@uidotdev/usehooks";
+import { useRouter } from "next/router";
 import { formatAddress } from "utils/formatting";
 import { useBalance } from "wagmi";
-import { useNetwork } from "wagmi";
-import { useRouter } from "next/router";
-import { useWallets } from "@privy-io/react-auth";
-import type { Chain } from "wagmi";
-import { useDisclosure } from "@chakra-ui/react";
+import ChainSwitcher from "./ChainSwitcher";
+
+
 
 const PrivyAuthButton = () => {
   const { login, ready, authenticated, user, logout } = usePrivy();
-  const { onOpen, onClose, isOpen } = useDisclosure();
-  const { chains, chain } = useNetwork();
+
   const { data } = useBalance({
     address: user?.wallet?.address as `0x${string}`,
   });
-  const { wallets } = useWallets();
+
   const router = useRouter();
   const [, copyToClipboard] = useCopyToClipboard();
 
-  const [chainName, setChainName] = useState<string>("" as string);
 
-  useEffect(() => {
-    if (chain) {
-      setChainName(chain.name);
-    }
-  }, [chain]);
 
   const address = user?.wallet?.address;
   if (!ready) return <Box>Loading...</Box>;
 
   if (authenticated) {
     return (
-      <Menu onClose={onClose}>
+      <Menu>
         <MenuButton
           as={Button}
           rightIcon={<ChevronDownIcon />}
@@ -62,72 +45,38 @@ const PrivyAuthButton = () => {
         >
           {`Logged in as ` + formatAddress(address ?? "")}
         </MenuButton>
-        <MenuList>
+        <MenuList p={2} background={"#242423"} textColor={'white'}>
           <MenuItem
             onClick={() => copyToClipboard(user?.wallet?.address as string)}
+            p={2}
+
+            bg={'transparent'}
+            rounded={'md'}
+            _hover={{ bgColor: 'green', color: 'dark-green', fontWeight: 'medium' }}
           >
             <CopyIcon mr={2} /> Copy Wallet Address
           </MenuItem>
-          <MenuItem>
-            Connected to: {chainName ? chainName : "Loading..."}
-          </MenuItem>
-          <Popover closeOnBlur={false} isOpen={isOpen} onClose={onClose}>
-            <PopoverTrigger>
-              <Button
-                as="div"
-                variant="unstyled"
-                fontWeight="bold"
-                textAlign="left"
-                p={0}
-                _hover={{ bg: "gray.100" }}
-                _focus={{ boxShadow: "none" }}
-                onClick={onOpen}
-              >
-                Switch Network
-              </Button>
-            </PopoverTrigger>
-            <Portal>
-              <PopoverContent>
-                <PopoverArrow />
-                <PopoverHeader>Select a Network</PopoverHeader>
-                <PopoverCloseButton />
-                <PopoverBody>
-                  {chains &&
-                    chains.map((chainItem: Chain) => {
-                      if (chainItem.name === chainName) {
-                        return null;
-                      }
-                      return (
-                        <MenuItem
-                          key={chainItem.name}
-                          onClick={async () => {
-                            if (wallets && wallets[0]) {
-                              try {
-                                await wallets[0].switchChain(chainItem.id);
-                                onClose();
-                              } catch (error) {
-                                console.log(error);
-                              }
-                            }
-                          }}
-                        >
-                          {chainItem.name}
-                        </MenuItem>
-                      );
-                    })}
-                </PopoverBody>
-              </PopoverContent>
-            </Portal>
-          </Popover>
 
-          <MenuItem>
-            {data?.formatted} {data?.symbol}
+          <MenuItem p={2}
+            bg={'transparent'}
+            rounded={'md'}
+            _hover={{ bgColor: 'green', color: 'dark-green', fontWeight: 'medium' }}>
+            {data?.formatted.substring(0, 8)} {data?.symbol}
           </MenuItem>
-          <MenuItem onClick={() => router.push("/my-hypercerts")}>
+          <ChainSwitcher />
+          <MenuItem
+            bg={'transparent'}
+            _hover={{ bgColor: 'green', color: 'dark-green', fontWeight: 'medium' }}
+            onClick={() => router.push("/my-hypercerts")}>
             {" "}
             My Hypercerts
-          </MenuItem>
-          <MenuItem onClick={logout}>Logout</MenuItem>
+          </MenuItem >
+          <MenuItem onClick={logout} p={2}
+            rounded={'md'}
+            bg={'transparent'}
+
+            _hover={{ bgColor: 'green', color: 'dark-green', fontWeight: 'medium' }}
+          >Logout</MenuItem>
         </MenuList>
       </Menu>
     );
