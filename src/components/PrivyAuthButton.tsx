5b6dfe38edb26a5476744c61120777491726ad34
psparacino <tufnelenterprises@gmail.com>
2023-09-26 23:04:58 -0400

    ability to change between networks

--- src/components/PrivyAuthButton.tsx
+++ src/components/PrivyAuthButton.tsx
@@ -1,4 +1,4 @@
-'use client'
+"use client";
 import { ChevronDownIcon, CopyIcon } from "@chakra-ui/icons";
 import {
   Box,
@@ -7,6 +7,14 @@ import {
   MenuButton,
   MenuItem,
   MenuList,
+  Popover,
+  PopoverTrigger,
+  PopoverContent,
+  PopoverBody,
+  PopoverArrow,
+  PopoverHeader,
+  Portal,
+  PopoverCloseButton,
 } from "@chakra-ui/react";
 import { useEffect, useState } from "react";
 import { usePrivy } from "@privy-io/react-auth";
@@ -15,50 +23,111 @@ import { formatAddress } from "utils/formatting";
 import { useBalance } from "wagmi";
 import { useNetwork } from "wagmi";
 import { useRouter } from "next/router";
+import { useWallets } from "@privy-io/react-auth";
+import type { Chain } from "wagmi";
+import { useDisclosure } from "@chakra-ui/react";
+
 const PrivyAuthButton = () => {
-  const [chainName, setChainName] = useState<string>("" as string);
   const { login, ready, authenticated, user, logout } = usePrivy();
-  
-  const router = useRouter();
-  const [,copyToClipboard] = useCopyToClipboard();
-  const { chain } = useNetwork();
+  const { onOpen, onClose, isOpen } = useDisclosure();
+  const { chains, chain } = useNetwork();
   const { data } = useBalance({
     address: user?.wallet?.address as `0x${string}`,
   });
+  const { wallets } = useWallets();
+  const router = useRouter();
+  const [, copyToClipboard] = useCopyToClipboard();
+
+  const [chainName, setChainName] = useState<string>("" as string);
 
   useEffect(() => {
-
     if (chain) {
       setChainName(chain.name);
     }
-  }
-  , [chain]);
+  }, [chain]);
 
   const address = user?.wallet?.address;
   if (!ready) return <Box>Loading...</Box>;
 
   if (authenticated) {
     return (
-      <Menu>
+      <Menu onClose={onClose}>
         <MenuButton
           as={Button}
           rightIcon={<ChevronDownIcon />}
           bgColor="dark-green"
-          color='green'
+          color="green"
           _hover={{ bgColor: "mid-green", textColor: "dark-green" }}
           _active={{ bgColor: "mid-green", textColor: "dark-green" }}
         >
           {`Logged in as ` + formatAddress(address ?? "")}
         </MenuButton>
-        <MenuList >
-          <MenuItem onClick={() => copyToClipboard(user?.wallet?.address as string)}><CopyIcon mr={2}/> Copy Wallet Address</MenuItem>
+        <MenuList>
+          <MenuItem
+            onClick={() => copyToClipboard(user?.wallet?.address as string)}
+          >
+            <CopyIcon mr={2} /> Copy Wallet Address
+          </MenuItem>
           <MenuItem>
-            Connected to: {chainName ? chainName : 'Loading...'}
+            Connected to: {chainName ? chainName : "Loading..."}
           </MenuItem>
-          <MenuItem>{data?.formatted} {data?.symbol}</MenuItem>
-          <MenuItem onClick={() => router.push('/my-hypercerts')}> My Hypercerts</MenuItem>
-          <MenuItem onClick={logout}>Logout 
+          <Popover closeOnBlur={false} isOpen={isOpen} onClose={onClose}>
+            <PopoverTrigger>
+              <Button
+                as="div"
+                variant="unstyled"
+                fontWeight="bold"
+                textAlign="left"
+                p={0}
+                _hover={{ bg: "gray.100" }}
+                _focus={{ boxShadow: "none" }}
+                onClick={onOpen}
+              >
+                Switch Network
+              </Button>
+            </PopoverTrigger>
+            <Portal>
+              <PopoverContent>
+                <PopoverArrow />
+                <PopoverHeader>Select a Network</PopoverHeader>
+                <PopoverCloseButton />
+                <PopoverBody>
+                  {chains &&
+                    chains.map((chainItem: Chain) => {
+                      if (chainItem.name === chainName) {
+                        return null;
+                      }
+                      return (
+                        <MenuItem
+                          key={chainItem.name}
+                          onClick={async () => {
+                            if (wallets && wallets[0]) {
+                              try {
+                                await wallets[0].switchChain(chainItem.id);
+                                onClose();
+                              } catch (error) {
+                                console.log(error);
+                              }
+                            }
+                          }}
+                        >
+                          {chainItem.name}
+                        </MenuItem>
+                      );
+                    })}
+                </PopoverBody>
+              </PopoverContent>
+            </Portal>
+          </Popover>
+
+          <MenuItem>
+            {data?.formatted} {data?.symbol}
           </MenuItem>
+          <MenuItem onClick={() => router.push("/my-hypercerts")}>
+            {" "}
+            My Hypercerts
+          </MenuItem>
+          <MenuItem onClick={logout}>Logout</MenuItem>
         </MenuList>
       </Menu>
     );
