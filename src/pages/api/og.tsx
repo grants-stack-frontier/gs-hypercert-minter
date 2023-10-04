bc5ba1bb31fcc96fcfce321c7e3d3674b0c9c7f0
psparacino <tufnelenterprises@gmail.com>
2023-10-04 10:03:08 -0400

    removed absolute urls for img src in api/og

--- src/pages/api/og.tsx
+++ src/pages/api/og.tsx
@@ -15,159 +15,156 @@ export const roboto_mono = Roboto_Mono({
 export default async function handler(req: Request) {
   const form = await req.json();
 
+  const { formData } = form as { formData: formSchema };
 
-    const { formData } = form as { formData: formSchema };
-    
+  if (!formData) {
+    return new Response("No form data provided", { status: 400 });
+  }
 
-    if(!formData) {
-      return new Response('No form data provided', { status: 400 })
-    }
-
-    const selectedChapter = Object.values(formData)?.[4] as unknown as optionType;
-    return new ImageResponse(
-      (
+  const selectedChapter = Object.values(formData)?.[4] as unknown as optionType;
+  return new ImageResponse(
+    (
+      <div
+        style={{
+          width: "100%",
+          height: "100%",
+          backgroundColor: "#254D32",
+          backgroundImage: `url('${process.env.NEXT_PUBLIC_DEPLOYMENT_IMAGES}/svgPatterns/certSvg.svg')`,
+          backgroundSize: "cover",
+          backgroundRepeat: "repeat-y",
+          justifyContent: "flex-end",
+          alignItems: "flex-start",
+          padding: "16px",
+          display: "flex",
+          flexDirection: "column",
+          gap: "0.5rem",
+          borderRadius: "10px",
+          overflow: "hidden",
+        }}
+      >
         <div
           style={{
             width: "100%",
-            height: "100%",
-            backgroundColor: "#254D32",
-            backgroundImage:
-              "url('https://gs-hypercert.vercel.app/svgPatterns/certSvg.svg')",
-            backgroundSize: "cover",
-            backgroundRepeat: "repeat-y",
-            justifyContent: "flex-end",
-            alignItems: "flex-start",
-            padding: "16px",
+            justifyContent: "center",
             display: "flex",
-            flexDirection: "column",
-            gap: "0.5rem",
-            borderRadius: "10px",
-            overflow: "hidden",
           }}
         >
-          <div
+          <img
+            src={`${process.env.NEXT_PUBLIC_DEPLOYMENT_IMAGES}/collection_logos/green-pill.png`}
+            alt=""
+            width="140"
+          />
+        </div>
+        <div
+          style={{
+            width: "100%",
+            justifyContent: "space-between",
+            display: "flex",
+            fontSize: "14px",
+            fontFamily: "Raleway, sans-serif",
+            fontWeight: "bolder",
+            color: "white",
+          }}
+        >
+          <p>Timeframe</p>
+          <p>
+            {formData.workTimeframeStart ?? "Start Date"}
+            {" — "}
+            {formData.workTimeframeEnd ?? "End Date"}
+          </p>
+        </div>
+
+        <hr
+          style={{
+            borderColor: "#C2E812",
+            borderWidth: "1px",
+            width: "100%",
+            margin: "5px 0",
+          }}
+        />
+
+        <div
+          style={{
+            display: "flex",
+            flexDirection: "column",
+            justifyContent: "space-between",
+            flex: 1,
+          }}
+        >
+          <h1
             style={{
-              width: "100%",
-              justifyContent: "center",
-              display: "flex",
-            }}
-          >
-            <img
-              src="https://gs-hypercert.vercel.app/collection_logos/green-pill.png"
-              alt=""
-              width="140"
-            />
-          </div>
-          <div
-            style={{
-              width: "100%",
-              justifyContent: "space-between",
-              display: "flex",
-              fontSize: "14px",
-              fontFamily: "Raleway, sans-serif",
-              fontWeight: "bolder",
               color: "white",
+              fontWeight: "bolder",
+              fontSize: "24px",
+              margin: "4px 5px 8px 5px",
+              fontFamily: volkhov.style.fontFamily,
             }}
           >
-            <p>Timeframe</p>
-            <p>
-              {formData.workTimeframeStart ?? 'Start Date'}
-              {" — "}
-              {formData.workTimeframeEnd ?? 'End Date'}
-            </p>
-          </div>
-
-          <hr
-            style={{
-              borderColor: "#C2E812",
-              borderWidth: "1px",
-              width: "100%",
-              margin: "5px 0",
-            }}
-          />
+            {selectedChapter?.label ?? "Name of the chapter"}
+          </h1>
 
           <div
             style={{
               display: "flex",
-              flexDirection: "column",
-              justifyContent: "space-between",
-              flex: 1,
+              flexWrap: "wrap",
+              gap: "8px",
             }}
           >
-            <h1
-              style={{
-                color: "white",
-                fontWeight: "bolder",
-                fontSize: "24px",
-                margin: "4px 5px 8px 5px",
-                fontFamily: volkhov.style.fontFamily,
-              }}
-            >
-              {selectedChapter?.label ?? "Name of the chapter"}
-            </h1>
-
-            <div
-              style={{
-                display: "flex",
-                flexWrap: "wrap",
-                gap: "8px",
-              }}
-            >
-              {formData?.workScope?.map(
-                (tagObject: { value: string; label: string }) => (
-                  <span
-                    key={tagObject.value}
-                    style={{
-                      color: "#C2E812",
-                      borderColor: "#C2E812",
-                      borderWidth: "1px",
-                      borderStyle: "solid",
-                      borderRadius: "10px",
-                      height: "auto",
-                      backgroundColor: "transparent",
-                      fontSize: "12px",
-                      fontWeight: "normal",
-                      display: "flex",
-                      alignItems: "center",
-                      marginTop: "4px",
-                      padding: "3px 10px",
-                    }}
-                  >
-                    {tagObject.label.toLowerCase()}
-                  </span>
-                ),
-              )}
-            </div>
-          </div>
-
-          <hr
-            style={{
-              borderColor: "#C2E812",
-              borderWidth: "1px",
-              width: "100%",
-              margin: "5px 0",
-            }}
-          />
-
-          <div
-            style={{
-              width: "100%",
-              justifyContent: "center",
-              display: "flex",
-              padding: "5px",
-            }}
-          >
-            <img
-              src="https://gs-hypercert.vercel.app/logo-yellow.svg"
-              alt="Hypercert"
-              width="140"
-            />
+            {formData?.workScope?.map(
+              (tagObject: { value: string; label: string }) => (
+                <span
+                  key={tagObject.value}
+                  style={{
+                    color: "#C2E812",
+                    borderColor: "#C2E812",
+                    borderWidth: "1px",
+                    borderStyle: "solid",
+                    borderRadius: "10px",
+                    height: "auto",
+                    backgroundColor: "transparent",
+                    fontSize: "12px",
+                    fontWeight: "normal",
+                    display: "flex",
+                    alignItems: "center",
+                    marginTop: "4px",
+                    padding: "3px 10px",
+                  }}
+                >
+                  {tagObject.label.toLowerCase()}
+                </span>
+              )
+            )}
           </div>
         </div>
-      ),
-      {
-        width: 320,
-        height: 400,
-      },
-    );
-  }
+
+        <hr
+          style={{
+            borderColor: "#C2E812",
+            borderWidth: "1px",
+            width: "100%",
+            margin: "5px 0",
+          }}
+        />
+
+        <div
+          style={{
+            width: "100%",
+            justifyContent: "center",
+            display: "flex",
+            padding: "5px",
+          }}
+        >
+          <img
+            src={`${process.env.NEXT_PUBLIC_DEPLOYMENT_IMAGES}/logo-yellow.svg`}
+            alt="Hypercert"
+            width="140"
+          />
+        </div>
+      </div>
+    ),
+    {
+      width: 320,
+      height: 400,
+    }
+  );
+}
