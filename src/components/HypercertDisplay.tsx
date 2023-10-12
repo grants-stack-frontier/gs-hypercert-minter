52ea1b24df368a03d0fa74b7b58f74a954bffcb2
psparacino <tufnelenterprises@gmail.com>
2023-10-12 10:52:57 -0400

    ux flow

--- src/components/HypercertDisplay.tsx
+++ src/components/HypercertDisplay.tsx
@@ -7,48 +7,53 @@ import { imageDataAtom } from "pages";
 import { useEffect } from "react";
 import type { formSchema } from "utils/types";
 
-
 export const HypercertDisplay = ({ formData }: { formData: formSchema }) => {
-    const [imageData, setImageData] = useAtom(imageDataAtom);
-    const queryClient = useQueryClient();
+  const [imageData, setImageData] = useAtom(imageDataAtom);
+  const queryClient = useQueryClient();
 
-    const mutation = useMutation<ArrayBuffer, Error, formSchema>(
-      (data: formSchema) => axios.post("/api/og", { formData: data }, { 
-        headers: {  'Content-Type': 'application/json'},
-        responseType: 'arraybuffer' })
+  const mutation = useMutation<ArrayBuffer, Error, formSchema>(
+    (data: formSchema) =>
+      axios
+        .post(
+          "/api/og",
+          { formData: data },
+          {
+            headers: { "Content-Type": "application/json" },
+            responseType: "arraybuffer",
+          }
+        )
         .then((response) => response.data as ArrayBuffer),
-      {
-        onSuccess: (data: ArrayBuffer) => {
-          const base64 = btoa(
-            new Uint8Array(data)
-              .reduce((acc, byte) => acc + String.fromCharCode(byte), '')
-          );
-          const encodedImage = `data:image/png;base64,${base64}`;
-          queryClient.setQueryData(["hypercertImage"], encodedImage);
-            setImageData(encodedImage);
-
-        }
-      }
-    );
-  
+    {
+      // onSuccess: (data: ArrayBuffer) => {
+      //   const base64 = btoa(
+      //     new Uint8Array(data).reduce(
+      //       (acc, byte) => acc + String.fromCharCode(byte),
+      //       ""
+      //     )
+      //   );
+      //   const encodedImage = `data:image/png;base64,${base64}`;
+      //   queryClient.setQueryData(["hypercertImage"], encodedImage);
+      //   setImageData(encodedImage);
+      // },
+      onSuccess: (data: ArrayBuffer) => {
+        const base64 = Buffer.from(data).toString("base64");
+        const encodedImage = `data:image/png;base64,${base64}`;
+        queryClient.setQueryData(["hypercertImage"], encodedImage);
+        setImageData(encodedImage);
+      },
+    }
+  );
 
   useEffect(() => {
     mutation.mutate(formData);
+    // eslint-disable-next-line react-hooks/exhaustive-deps
   }, [formData]);
-    
+
   return (
     <VStack>
-
       {imageData && (
-        <Image
-          src={imageData}
-          alt="Hypercert Image"
-          width={320} 
-          height={400} 
-        />
+        <Image src={imageData} alt="Hypercert Image" width={320} height={400} />
       )}
-
-
     </VStack>
   );
 };
