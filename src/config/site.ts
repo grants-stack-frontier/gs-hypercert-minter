44dee95a9b12478a36571cdf98188f55ee64c837
jipstavenuiter <jipstavenuiter@gmail.com>
2023-10-13 13:29:06 +0200

    fix site title

--- src/config/site.ts
+++ src/config/site.ts
@@ -1,5 +1,5 @@
 const site = {
-  title: "Green Pill Network Impactooorrr",
+  title: "Green Pill Network",
   description:
     "We’re building a CoordiNation across Nations & Cultures. If you did something regen, mint yourself or team a Greenpill hypercert to show your proof of impact",
   url: "https://GreenPill.network",
