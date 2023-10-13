df226988fdbc8ccfd947e4336b5bd4d4fea7d9e1
psparacino <tufnelenterprises@gmail.com>
2023-10-13 18:17:40 -0400

    more connection button logic rework

--- src/hooks/useAuthenticationAndCorrectChain.ts
+++ src/hooks/useAuthenticationAndCorrectChain.ts
@@ -5,11 +5,13 @@ import { useNetwork } from "wagmi";
 import { usePrivyWagmi } from "@privy-io/wagmi-connector";
 
 export const useAuthenticationAndChainCheck = () => {
-  const { authenticated, user } = usePrivy();
+  const { ready, authenticated } = usePrivy();
   const { chains } = useNetwork();
-  const { wallet: activeWallet } = usePrivyWagmi();
+  const { wallet: activeWallet, ready: privyWagmiReady } = usePrivyWagmi();
 
   const authenticatedAndCorrectChain = useMemo(() => {
+
+
     if (!activeWallet || !activeWallet.chainId) {
       return "";
     }
@@ -19,14 +21,16 @@ export const useAuthenticationAndChainCheck = () => {
       id = parseInt(activeWallet.chainId.split(":")[1]!, 10);
     }
     if (
-      user &&
+      ready &&
+      privyWagmiReady &&
       authenticated &&
+      id &&
       !!chains.find((connectedChain) => connectedChain.id === id)
     ) {
       return "settled";
     }
     return "";
-  }, [user, authenticated, chains, activeWallet]);
+  }, [ready, privyWagmiReady, authenticated, chains, activeWallet]);
 
   return authenticatedAndCorrectChain;
 };
